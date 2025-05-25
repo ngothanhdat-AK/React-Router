637791f0b571fa2561d58d15e1ff665770d88265
ngothanhdat-AK <ngothanhdat4002@gmail.com>
2025-05-26 03:59:41 +0700

    refactor: clean up unused imports in Layout component

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,8 +1,4 @@
-import {Stack, Button} from "react-bootstrap";
-import {Routes, Route, Outlet} from "react-router-dom";
-import Home from "../pages/Home";
-import Blog from "../pages/Blog/Blog";
-import Contact from "../pages/Contact/Contact";
+import {Outlet} from "react-router-dom";
 import Footer from "../pages/Footer/Footer";
 import Header from "../pages/Header/Header";
 
